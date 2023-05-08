4c283188e20f9cf5cb5bc032d89f3fe1fe03a15c
vpasemko <vpasem@softserveinc.com>
2023-05-08 15:43:30 +0300

    first commit

---
+++ src/main/java/com/university/botscrewtesttask/BotsCrewTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.university.botscrewtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotsCrewTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotsCrewTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/botscrewtesttask/BotsCrewTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.botscrewtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotsCrewTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
